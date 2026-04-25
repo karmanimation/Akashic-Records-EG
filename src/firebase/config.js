f18bdbf7a94c6499f1de43ca07cb29939cff44cf
karmanimation <karmanimationsk9@gmail.com>
2026-04-25 01:17:05 -0300

    config firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAV-l08jrFh-CbDNnOvucYJepcVTOPUrHg",
+  authDomain: "entregalaxias369.firebaseapp.com",
+  projectId: "entregalaxias369",
+  storageBucket: "entregalaxias369.firebasestorage.app",
+  messagingSenderId: "1042139733115",
+  appId: "1:1042139733115:web:27db1af4e44e07321f56de",
+  measurementId: "G-HH07FG84JD"
 }
 
 const app = initializeApp(firebaseConfig)
